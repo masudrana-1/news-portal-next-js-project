86e4523613f916a45f02cd180d1e690f5f6cb7e5
Masud Rana <masudrana58855@gmail.com>
2023-10-18 20:56:27 +0600

    create dynamic route api

--- src/pages/news/[newsId].js
+++ src/pages/news/[newsId].js
@@ -1,9 +1,10 @@
 import RootLayout from "@/components/Layouts/RootLayout";
 
-const NewsDetailsPage = () => {
+const NewsDetailsPage = ({news}) => {
     return (
         <div>
-            <h1>This is news details page</h1>
+            <h1>{news?.title}</h1>
+            <h3>{news?.description}</h3>
         </div>
     );
 };
@@ -14,3 +15,38 @@ NewsDetailsPage.getLayout = function getLayout(page) {
     return <RootLayout>{page}</RootLayout>;
   };
   
+
+// for dynamic route 
+export const getStaticPaths = async () => {
+    const res = await fetch("http://localhost:5000/news")
+    const newses = await res.json();
+
+
+    const paths = newses.map((news)=>({
+        params: {newsId: news.id},
+    }));
+
+
+    return {
+        paths, 
+        fallback: false
+    }
+}
+
+
+
+export const getStaticProps = async (context) =>{
+
+    const {params} = context;
+
+    const res = await fetch(`http://localhost:5000/news/${params.newsId}`)
+    const data = await res.json();
+
+    console.log(data)
+
+    return {
+        props: {
+            news: data
+        },
+    }
+}
