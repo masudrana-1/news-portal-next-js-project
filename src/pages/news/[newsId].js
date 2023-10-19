921633caba8f14fdb53b255f52f038eef24a9fce
Masud Rana <masudrana58855@gmail.com>
2023-10-19 10:52:23 +0600

    news details page fixed

--- src/pages/news/[newsId].js
+++ src/pages/news/[newsId].js
@@ -5,72 +5,69 @@ import {
     CalendarOutlined,
     CommentOutlined,
     ProfileOutlined,
+    UserOutlined
   } from "@ant-design/icons";
 
 
 const NewsDetailsPage = ({news}) => {
     return (
-        <div>
-            <Row
-                gutter={{
-                    xs: 8,
-                    sm: 16,
-                    md: 24,
-                    lg: 32,
-                }}
-                >
-                <Col className="gutter-row" span={12}>
-                    <div>
-                        <Image
-                        src={news?.image_url}
-                        width={500}
-                        height={200}
-                        responsive
-                        alt="news image"
-                        />
-                    </div>
-                </Col>
-                <Col className="gutter-row" span={12}>
-                    <div>
-                        <h1 style={{fontSize: "25"}}>{news.title}</h1>
-                        <div
-                            className="line"
-                            style={{
-                            height: "5px",
-                            margin: "20px 0",
-                            background: "#000",
-                            width: "100%",
-                            }}
-                        ></div>
-
-                        <p
-                            style={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                            width: "100%",
-                            color: "gray",
-                            margin: "10px 0px",
-                            fontSize: "12px",
-                            }}
+        <Row style={{ marginTop: "80px", alignItems: "center" }}>
+            <Col md={6} lg={12}>
+                <Image
+                    alt="example"
+                    src={news?.image_url}
+                    width={500}
+                    height={300}
+                    responsive
+                />
+            </Col>
+            <Col md={6} lg={12} style={{ paddingLeft: "20px" }}>
+                <h1 style={{ fontSize: "30px" }}>{news?.title}</h1>
+                    <span
+                        style={{
+                        color: "gray",
+                        display: "block",
+                        fontSize: "20px",
+                        }}
                         >
-                            <span>
-                            <CalendarOutlined /> {news?.release_date}
-                            </span>
-                            <span>
-                            <CommentOutlined /> {news?.comment_count} COMMENTS
-                            </span>
-                            <span>
-                            <ProfileOutlined /> {news?.category}
-                            </span>
-                        </p>
+                        <UserOutlined /> {news?.author}
+                    </span>
+                <div
+                    className="line"
+                    style={{
+                    height: "5px",
+                    margin: "20px 0",
+                    background: "#000",
+                    width: "100%",
+                    }}
+                ></div>
 
-                        <p style={{ fontSize: "20px" }}>
-                            {news?.description}
-                        </p>
-                    </div>
-                </Col>
-            </Row>
-        </div>
+                <p
+                    style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    width: "100%",
+                    color: "gray",
+                    margin: "10px 0px",
+                    fontSize: "20px",
+                    }}
+                >
+                    <span>
+                    <CalendarOutlined /> {news?.release_date}
+                    </span>
+                    <span>
+                    <CommentOutlined /> {news?.comment_count} Comments
+                    </span>
+                    <span>
+                    <ProfileOutlined /> {news?.category}
+                    </span>
+                </p>
+
+                <p style={{ fontSize: "25px", fontWeight: "lighter" }}>
+                    {news?.description}
+                </p>
+            </Col>
+        </Row>
     );
 };
 
