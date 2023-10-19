a1db0ce080313021617b120fe86a7d0d8fa368ab
Masud Rana <masudrana58855@gmail.com>
2023-10-19 10:14:47 +0600

    complete static site redering

--- src/pages/news/[newsId].js
+++ src/pages/news/[newsId].js
@@ -1,10 +1,75 @@
 import RootLayout from "@/components/Layouts/RootLayout";
+import { Col, Image, Row } from "antd";
+import {
+    ArrowRightOutlined,
+    CalendarOutlined,
+    CommentOutlined,
+    ProfileOutlined,
+  } from "@ant-design/icons";
+
 
 const NewsDetailsPage = ({news}) => {
     return (
         <div>
-            <h1>{news?.title}</h1>
-            <h3>{news?.description}</h3>
+            <Row
+                gutter={{
+                    xs: 8,
+                    sm: 16,
+                    md: 24,
+                    lg: 32,
+                }}
+                >
+                <Col className="gutter-row" span={12}>
+                    <div>
+                        <Image
+                        src={news?.image_url}
+                        width={500}
+                        height={200}
+                        responsive
+                        alt="news image"
+                        />
+                    </div>
+                </Col>
+                <Col className="gutter-row" span={12}>
+                    <div>
+                        <h1 style={{fontSize: "25"}}>{news.title}</h1>
+                        <div
+                            className="line"
+                            style={{
+                            height: "5px",
+                            margin: "20px 0",
+                            background: "#000",
+                            width: "100%",
+                            }}
+                        ></div>
+
+                        <p
+                            style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            width: "100%",
+                            color: "gray",
+                            margin: "10px 0px",
+                            fontSize: "12px",
+                            }}
+                        >
+                            <span>
+                            <CalendarOutlined /> {news?.release_date}
+                            </span>
+                            <span>
+                            <CommentOutlined /> {news?.comment_count} COMMENTS
+                            </span>
+                            <span>
+                            <ProfileOutlined /> {news?.category}
+                            </span>
+                        </p>
+
+                        <p style={{ fontSize: "20px" }}>
+                            {news?.description}
+                        </p>
+                    </div>
+                </Col>
+            </Row>
         </div>
     );
 };
@@ -15,6 +80,9 @@ NewsDetailsPage.getLayout = function getLayout(page) {
     return <RootLayout>{page}</RootLayout>;
   };
   
+
+//! ************************************************************************************* */
+//! SSR = static site rendering 
 
 // for dynamic route 
 export const getStaticPaths = async () => {
@@ -50,3 +118,5 @@ export const getStaticProps = async (context) =>{
         },
     }
 }
+
+//! ************************************************************************************* */
