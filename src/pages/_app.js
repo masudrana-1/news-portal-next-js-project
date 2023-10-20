543806e001abc6ce9f706e086f76d84f0b7c9449
Masud Rana <masudrana58855@gmail.com>
2023-10-20 10:06:42 +0600

    add redux and crate store

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,14 @@
+import { store } from '@/redux/store';
 import '@/styles/globals.css'
+import { Provider } from 'react-redux';
 
 export default function App({ Component, pageProps }) {
 
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  // implement redux 
+
+  return <Provider store={store}>
+              {getLayout(<Component {...pageProps} />)}
+        </Provider>
 }
